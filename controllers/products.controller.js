3a92e4a0ab7ab352b5eb94887d4c5cc830ff18d4
evengyl <dark.evengyl@gmail.com>
2022-02-14 16:37:37 +0100

    imp code express with mysql

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,28 +1,58 @@
+const db = require("../models/dbConnect").get()
+
+/*
+    localhost:3000/api/products/            --> get all
+    localhost:3000/api/products/:id         --> get one by id
+    localhost:3000/api/products/            --> post product
+    localhost:3000/api/products/:id         --> put / patch product
+    localhost:3000/api/products/:id         --> delete one product
+*/
+
+
 exports.getAllProducts =  (req, res) => {
-    res.json({
-        message : "Listes de tous les produits"
+    
+    db.then((connection) => {
+        connection.query("select * from products").then((datas) => {
+            res.json(datas)
+        })
     })
+
+
 }
 
 
 exports.getOneProduct = (req, res) => {
-    res.json({
-        message : "le produit que vous avez demandé"
+
+    let id = req.params.id
+    db.then((connection) => {
+        connection.query("select * from products where id = ?", [id]).then((datas) => {
+            res.json(datas)
+        })
     })
+
 }
 
 
 exports.postProduct = (req, res) => {
-    res.json({
-        message : "Produit créé"
+
+    let name = req.body.name
+    let price = req.body.price
+
+    db.then((connection) => {
+        //j'ai, à cet endroit, l'objet de connection, le lien à la db,
+        //je vais faire un insert into, dans products, j'ai besoin de lui donner name et price -> (name, price)
+        //avec les valeurs (?, ?) 
+        //pour sécuriser la requète, je lui donne un tableau contenant par ordre !!!! les paramètres demandé
+        //donc -> [name, price], le premier ? contiendra name, et le deuxième ? contiendra price
+        connection.query("insert into products (name, price) values (?, ?)", [name, price]).then((result) => {
+            res.json(result)
+        })
     })
 }
 
 
 exports.putProduct = (req, res) => {
-    res.json({
-        message : "Produit modifié"
-    })
+    
 }
 
 
